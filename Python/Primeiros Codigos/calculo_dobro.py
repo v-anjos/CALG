e848cd48df4c5817c9cf859a415c6f0e7457a573
Vinicius de Araujo dos Anjos <vanjos990@gmail.com>
2025-10-18 03:12:56 -0300

    Update and rename Python/calculo_dobro.py to Python/Primeiros Codigos/calculo_dobro.py

---
+++ Python/Primeiros Codigos/calculo_dobro.py
@@ -0,0 +1,10 @@
+# Programa para calcular o dobro de um número
+
+# Lendo o número
+numero = int(input("Digite um número: "))
+
+# Calculando o dobro
+dobro = numero * 2
+
+# Mostrando o resultado
+print(f"O dobro é {dobro}")
