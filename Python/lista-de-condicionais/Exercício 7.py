3fde6285df3b5865d8cc445fbed3d95357d52a4e
Vinicius de Araujo dos Anjos <vanjos990@gmail.com>
2026-02-17 21:57:20 -0300

    Create Exercício 7.py

---
+++ Python/lista-de-condicionais/Exercício 7.py
@@ -0,0 +1,8 @@
+temperatura = float(input("Digite a temperatura: "))
+
+if temperatura < 20:
+    print("Muito Frio")
+elif temperatura <= 30:
+    print("Clima agradável")
+else:
+    print("Muito quente")
