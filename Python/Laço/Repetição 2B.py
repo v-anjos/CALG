361170272f8abb443b85dfb2e01646646625ca20
Vinicius de Araujo dos Anjos <vanjos990@gmail.com>
2026-02-18 12:33:02 -0300

    Create Repetição 2B.py

---
+++ Python/Laço/Repetição 2B.py
@@ -0,0 +1,14 @@
+for contador in range(10):
+    valor = float(input("Digite um valor: "))
+
+    if contador == 0:
+        maior = valor
+        menor = valor
+    else:
+        if valor > maior:
+            maior = valor
+        elif valor < menor:
+            menor = valor
+
+print("O maior valor digitado é:", maior)
+print("O menor valor digitado é:", menor)
