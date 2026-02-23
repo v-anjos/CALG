70cef56868b7c37eaa20c05785d79390bde76a1d
Vinicius de Araujo dos Anjos <vanjos990@gmail.com>
2026-02-23 00:26:10 -0300

    Create Lista funçao 5.py

---
+++ Python/Função/Lista funçao 5.py
@@ -0,0 +1,16 @@
+def soma(a, b):
+
+    resultado = a + b
+
+    return resultado
+
+def inicio():
+
+    x = float(input("Digite um numero: "))
+    y = float(input("Digite um numero: "))
+
+    resultado = soma(x, y)
+
+    print("O resultado da soma é:", resultado)
+
+inicio()
